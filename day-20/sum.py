b65c73b8733b03cb28cfa84fafec452a052dda24
prathampai0667 <prathampai0667@gmail.com>
2026-08-01 06:52:04 +0530

    day-20 done

---
+++ day-20/sum.py
@@ -0,0 +1,11 @@
+import arithmatic
+import greet
+
+print(arithmatic.add_numbers(1, 2, 3, 5))
+print(arithmatic.subtract(5, 3))
+print(arithmatic.multiple(5, 3))
+print(arithmatic.division(5, 3))
+print(arithmatic.remainder(5, 3))
+print(arithmatic.power(5, 3))
+
+print(greet.greet_person("Anvesha", "Pratham"))
